fd2d6a1d1096c31c0fea387afcd564bd181585ab
David Allen <david.allen@columnzero.com>
2026-04-25 00:03:22 +0000

    ci: add missing toolchain dependencies to manifest and use guix shell for lint/test

--- manifest.scm
+++ manifest.scm
@@ -1,10 +1,14 @@
 (use-modules (guix packages)
              (gnu)
              (gnu packages rust)
-             (gnu packages pkg-config))
+             (gnu packages pkg-config)
+             (gnu packages commencement)
+             (gnu packages version-control))
 
 (packages->manifest (list pkg-config
                           rust
                           (list rust "cargo")
                           (list rust "tools")
-                          rust-analyzer))
+                          rust-analyzer
+                          gcc-toolchain
+                          git-minimal))
